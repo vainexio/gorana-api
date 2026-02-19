35644de971efe60b94a64699952d4e1f5675a294
Ian <111634400+vainexio@users.noreply.github.com>
2026-02-19 22:31:26 +0800

    Update build.ts

--- script/build.ts
+++ script/build.ts
@@ -30,7 +30,7 @@ async function buildAll() {
   const externals = allDeps.filter((dep) => !allowlist.includes(dep));
 
   await esbuild({
-    entryPoints: ["server/index.ts"],
+    entryPoints: ["server/index.js"],
     platform: "node",
     bundle: true,
     format: "cjs",
